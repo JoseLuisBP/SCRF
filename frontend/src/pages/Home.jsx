63756bfee72ed7cec587484489c70b25bed32ee1
Jose Luis Becerra <jose.becerra4633@alumnos.udg.mx>
2025-10-04 20:05:21 -0600

    Creación de ejemplo para la home page con stilos

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
+
+export default function Home() {
+    return (
+        <div className="home-container">
+            <section className="home-header">
+                <h1>One day, you'll be cool</h1>
+            </section>
+            <section className="home-content">
+                <p>Suena música de edit de tiktok</p>
+            </section>
+        </div>
+    );
+}
