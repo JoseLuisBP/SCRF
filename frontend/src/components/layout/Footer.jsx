ea0411e2bc855222944fdfbdfa7dae5fd26f0dee
fatimamdiaz-max <fatima.mdiaz@alumnos.udg.mx>
2025-10-21 16:37:31 -0600

    Creacion del Footer.jsx

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer(){
+    return(
+         
+        <footer className="bg-gray-800 text-white text-center p-4 mt-10">
+        <p>© 2025 Sistema Accesible. Todos los derechos reservados.</p>
+            <p>Contacto: reabilitacion@udg.com</p>
+             <div className="flex justify-center gap-4 mt-2">
+               <a href="https://facebook.com" target="_blank" rel="noreferrer">Facebook</a> 
+               <a href="https://facebook.com" target="_blank" rel="noreferrer">Facebook</a> 
+                <a href="https://twitter.com" target="_blank" rel="noreferrer">Twitter</a> 
+                <a href="https://instagram.com" target="_blank" rel="noreferrer">Instagram</a> 
+
+            </div>
+
+         </footer>
+    );
+}
+
+export default Footer;
