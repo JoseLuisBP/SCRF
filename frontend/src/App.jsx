493e0acd7f0651af4db1cad32f35907e41791f43
fatimamdiaz-max <fatima.mdiaz@alumnos.udg.mx>
2025-10-21 20:18:01 -0600

    se agrego comandos para Footer.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { AccessibilityProvider } from './context/AccessibilityContext';
 
+
 import theme from './styles/theme';
 import AppRoutes from './routes';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
+
     /* Proveedor de tema para MUI */
     <ThemeProvider theme={theme}>
       {/* Restablece los estilos base de MUI */}
@@ -18,12 +21,33 @@ function App() {
         <AccessibilityProvider>
           {/* Enrutador de la aplicación */}
           <BrowserRouter>
+
+          <div 
+          style={{
+            display:"flex",
+            flexDirection: "column",
+            minHeight:"100vh",
+          }}
+          >
+            {/*Contenido Principal */}
+            <main style={{ flex: 1, padding: "2rem", textAlign: "center"}}>
+              <h1>Pagina de rehabilitacion</h1>
+           
             {/* Rutas de la aplicación */}
             <AppRoutes />
+             </main>
+             <Footer />
+          </div>
           </BrowserRouter>
         </AccessibilityProvider>
       </AuthProvider>
     </ThemeProvider>
+
+
+    
+
+ 
+
   );
 }
 
