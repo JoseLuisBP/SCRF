7f5bbe23879e9980a0133818823b1abc3b0c7fd7
fatimamdiaz-max <fatima.mdiaz@alumnos.udg.mx>
2025-10-21 20:16:14 -0600

    modificar

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,48 +1,44 @@
 import React from "react";
-
+import { Box, Typography, IconButton } from "@mui/material";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
 
 function Footer() {
   return (
-    <footer className="bg-blue-900 text-white text-center p-6 mt-10">
-      <div className="max-w-6xl mx-auto ">
-        {/* Información básica */}
-        <h3 className="text-lg font-semibold m-2">
-          Pagina de Rehabilitación 
-        </h3>
-        <p className="text-sm m-4">
-          Ejercicios para todas las personas
-        </p>
+    <Box
+      sx={{
+        backgroundColor: "#1976d2",
+        color: "white",
+        textAlign: "center",
+        py: 3,
+        mt: "auto",
+      }}
+    >
+      <Typography variant="h6">Pagina de rehabilitacion </Typography>
+      <Typography variant="body2" sx={{ mb: 1 }}>
+        © {new Date().getFullYear()} Todos los derechos reservados
+      </Typography>
 
-        {/* Enlaces rápidos */}
-        <div className="flex justify-center space-x-6">
-          <a href="/sobre-nosotros" className="hover:underline">
-            Nosotros
-          </a>
-          <a href="/servicios" className="hover:underline">
-            Servicios
-          </a>
-          <a href="/contacto" className="hover:underline">
-            Contacto
-          </a>
-           <a href="/privacidad" className="hover:underline">
-            Política de Privacidad
-            </a>
-        </div>
-
+      {/* Iconos de redes sociales */}
+      <Box>
+        <IconButton
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+        >
+          <Facebook />
+        </IconButton>
+        <IconButton
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+        >
+          <Instagram />
+        </IconButton>
         
-        {/* Información de contacto */}
-        <p className="text-sm">
-           
-            33 254222
-         rehabilitacion@udg.mx
-        </p>
-
-        {/* Derechos de autor */}
-        <p className="text-xs mt-4">
-          © {new Date().getFullYear()} Pagina de Rehabilitación. Todos los derechos reservados.
-        </p>
-      </div>
-    </footer>
+      </Box>
+    </Box>
   );
 }
 
