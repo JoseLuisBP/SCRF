458a2deab8704f36d08575e13511097c346f81d7
Jose Luis Becerra <jose.becerra4633@alumnos.udg.mx>
2025-10-25 23:34:32 -0600

    Update VITE_API_URL in docker-compose and clean up App component formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { AccessibilityProvider } from './context/AccessibilityContext';
 
-
 import theme from './styles/theme';
 import AppRoutes from './routes';
 
 function App() {
   return (
-
     /* Proveedor de tema para MUI */
     <ThemeProvider theme={theme}>
       {/* Restablece los estilos base de MUI */}
@@ -20,18 +18,12 @@ function App() {
         <AccessibilityProvider>
           {/* Enrutador de la aplicación */}
           <BrowserRouter>
-          {/* Rutas de la aplicación */}
-          <AppRoutes />
+            {/* Rutas de la aplicación */}
+            <AppRoutes />
           </BrowserRouter>
         </AccessibilityProvider>
       </AuthProvider>
     </ThemeProvider>
-
-
-
-
-
-
   );
 }
 
