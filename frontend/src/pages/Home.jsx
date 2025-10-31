bc12baeb798e7a30be09a9c23acfca470a75265a
fatimamdiaz-max <fatima.mdiaz@alumnos.udg.mx>
2025-10-30 20:14:01 -0600

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -51,12 +51,12 @@ export default function Home() {
               flexBasis: {
                 xs: '100%',
                 sm: fontSize > 18 ? '100%' : '45%',
-                md: fontSize > 20 ? '100%' : '45%',
+                md: fontSize > 20 ? '100%' : '50%',
               },
               maxWidth: {
                 xs: '100%',
                 sm: fontSize > 18 ? '100%' : '45%',
-                md: fontSize > 20 ? '100%' : '45%',
+                md: fontSize > 20 ? '100%' : '50%',
               },
               flexGrow: 1,
             }}
@@ -109,12 +109,12 @@ export default function Home() {
               flexBasis: {
                 xs: '100%',
                 sm: fontSize > 18 ? '100%' : '45%',
-                md: fontSize > 20 ? '100%' : '45%',
+                md: fontSize > 20 ? '100%' : '50%',
               },
               maxWidth: {
                 xs: '100%',
                 sm: fontSize > 18 ? '100%' : '45%',
-                md: fontSize > 20 ? '100%' : '45%',
+                md: fontSize > 20 ? '100%' : '50%',
               },
               flexGrow: 1,
             }}
