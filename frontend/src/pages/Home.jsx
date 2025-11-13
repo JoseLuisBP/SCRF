7249ab5a1dfd5086b6d4508cdc092008fdb955e9
fatimamdiaz-max <fatima.mdiaz@alumnos.udg.mx>
2025-11-12 21:01:01 -0600

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,14 +6,12 @@ import Card from '../components/layout/Card';
 import { useAuth } from '../context/AuthContext';
 import { useAccessibility } from '../context/AccessibilityContext';
 
+{/*Librerias de Carrusel */}
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-
-
-
+{/*Imagenes*/}
 import gimnasioImg from '../assets/images/gimnasio.jpg';
 import ejercicioImg from '../assets/images/ejercicios.jpg';
 import onuImg from '../assets/images/onu.jpg';
@@ -21,6 +19,9 @@ import cuerpoImg from '../assets/images/cuerpo.jpg';
 import cuerpo2Img from '../assets/images/cuerpo2.jpg';
 import cuerpo3Img from '../assets/images/cuerpo3.jpg';
 import funcionImg from '../assets/images/funcion.jpg';
+import nosotrosImg from '../assets/images/nosotros.jpg';
+import beneficioImg from '../assets/images/beneficio.jpg';
+import contactoImg from '../assets/images/contacto.jpg';
 
 import Footer from '../components/layout/Footer';
 
@@ -55,11 +56,98 @@ export default function Home() {
       <Header showSearchBar={false} />
 
       <Container maxWidth="lg" sx={{ mt: 12, mb: 4 }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+            gap: fontSize > 18 ? 5 : 3,
+          }}
+        >
+          <Box
+            sx={{
+              position: 'relative',
+              width: '100%',
+              maxWidth: '1000px',
+              height: '450px',
+              borderRadius: '16px',
+              overflow: 'hidden',
+              boxShadow: '0 6px 18px rgba(0,0,0,0.25)',
+            }}
+          >
+            {/* Carrusel de imágenes */}
+            <Slider
+              autoplay
+              autoplaySpeed={2500}
+              dots={false}
+              arrows={false}
+              infinite
+              speed={800}
+              slidesToShow={1}
+              slidesToScroll={1}
+            >
+              {[cuerpoImg, cuerpo2Img, cuerpo3Img,onuImg,ejercicioImg].map((img, i) => (
+                <Box
+                  key={i}
+                  component="img"
+                  src={img}
+                  alt={`Imagen ${i + 1}`}
+                  sx={{
+                    width: '100%',
+                    height: '450px',
+                    objectFit: 'cover',
+                  }}
+                />
+              ))}
+            </Slider>
 
-      
+            {/* Texto fijo sobre las imágenes del carrusel*/}
+            <Box
+              sx={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                background: 'rgba(0, 0, 0, 0.45)',
+                color: 'white',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                px: 3,
+              }}
+            >
+              <Typography
+                variant="h4"
+                sx={{
+                  mb: 2,
+                  fontWeight: 'bold',
+                  textShadow: '2px 2px 4px rgba(0,0,0,0.6)',
+                  fontSize: { xs: '1.6rem', md: '2rem' },
+                }}
+              >
+               “Tu cuerpo no necesita un gimnasio, solo ganas de moverse.”
+              </Typography>
 
+              <Typography
+                variant="body1"
+                sx={{
+                  maxWidth: '700px',
+                  fontSize: { xs: '1rem', md: '1.2rem' },
+                  textShadow: '1px 1px 3px rgba(0,0,0,0.5)',
+                }}
+              >
+                ¿Quieres sentirte mejor, tener más energía y cuidar tu salud sin salir de casa?
+                ¡Estás en el lugar correcto! En  <b>esta pagina</b> encontrarás rutinas simples,
+                efectivas y adaptadas para cualquier persona, sin importar la edad o condición física.
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
        
-          
+       {/*Seccion de Inicio*/}   
           <Box id="inicio" sx={{ textAlign: 'center', mb: 8 }}>
           <Box
             id="sobre-nosotros"
@@ -72,12 +160,10 @@ export default function Home() {
             
           }}
         >
-           
           <Card
-            
             title="Sobre Nosotros"
-            description=" ¿Quieres sentirte mejor, tener mas energia y cuida tu salud sin salir de casa ? ¡Estas en el lugar correcto! Activate en casa encontraras rutinas simples, efectivas y adaptadas para cualquier persona, sin importar la edad o condicion fisica."
-             image={cuerpoImg}
+            description=" Nuestro objetivo es mejorar la calidad de vida de distintos sectores de la población: adultos mayores, personas en recuperación física y aquellos que buscan mantener un estilo de vida saludable y prevenir riesgos futuros. Que ayude a la conservación y el fortalecimiento de capacidades físicas, se reducen riesgos de salud tanto físicos como emocionales, fomentando la independencia y bienestar. Generando asimismo, beneficios indirectos en las familias. "
+            image= {nosotrosImg}
             sx={{
               flexBasis: {
                 xs: '100%',
@@ -92,31 +178,9 @@ export default function Home() {
               flexGrow: 1,
             }}
           />
+            </Box>
 
-        /*Carrsel de imagenes 
-          <Box sx={{ width: '90%', mt: 4}}>
-            <Slider {...sliderSettings}>
-              {[cuerpoImg,cuerpo2Img,cuerpo3Img,funcionImg].map((img,index)=>(
-                <Box key={index}>
-                  <img 
-                  src={img}
-                  alt={'Imagen ${index + 1}'}
-                  style={{
-                    width: '100%',
-                    height: '400%',
-                    objectFit: 'cover',
-                    borderRadius: '16px',
-                  }}
-                    />
-                    </Box>
-              ))}
-            </Slider>
-           </Box> 
-          </Box>
-
-
-         
-          
+         {/*Seccion Beneficios */}   
                <Box
             id="beneficios"
           sx={{
@@ -130,8 +194,14 @@ export default function Home() {
         >
            <Card
             title="Beneficios"
-            description="Es un sistema accesible que facilite la recuperación, conservación y fortalecimiento de movilidad, fuerza y flexibilidad en adultos mayores, personas en rehabilitación física y población interesada en prevenir riesgos de salud."
-            image={ejercicioImg}
+            description=" ¿Buscas mejorar tu salud física sin salir de casa? Nuestra página web te ofrece una solución completa con ejercicios diseñados especialmente para la rehabilitación y el bienestar. Aquí tienes lo que te espera:
+            • 	 Comodidad total: Realiza rutinas adaptadas desde tu hogar, sin necesidad de equipo especializado.
+            • 	 Ejercicios guiados por expertos: Videos y planes creados por profesionales en fisioterapia y entrenamiento funcional.
+            • 	 Rehabilitación progresiva: Programas pensados para recuperar movilidad, fuerza y confianza paso a paso.
+            • 	 A tu ritmo: Accede a las sesiones cuando quieras, sin presiones ni horarios fijos.
+            • 	 Seguimiento personalizado: Herramientas para monitorear tu avance y ajustar tus rutinas según tus necesidades.
+            • 	 Bienestar integral: Mejora tu calidad de vida, reduce el dolor y fortalece tu cuerpo con seguridad. "
+            image={beneficioImg}
             sx={{
               flexBasis: {
                 xs: '100%',
@@ -147,7 +217,8 @@ export default function Home() {
             }}
           />
           </Box>
-
+        
+        {/*Seccion de Contacto*/}
              <Box
             id="contacto"
           sx={{
@@ -159,12 +230,10 @@ export default function Home() {
             
           }}
         >
-         
-
           <Card
-            title="contacto"
-            description=" La importancia de la actividad física en la prevención  de enfermedades crónicas y en la mejora de la calidad de vida en adultos mayores. Según la Organización Mundial de la Salud (OMS), el envejecimiento activo se basa en la optimización de las oportunidades de salud, participación y seguridad. Proyectos tecnológicos similares, como aplicaciones de fisioterapia o plataformas de ejercicio, suelen carecer de una orientación especializada hacia personas mayores o en rehabilitación, por lo que el presente sistema busca cubrir esa brecha mediante un enfoque inclusivo, accesible y sustentado en inteligencia artificial."
-            image={onuImg}
+            title="Contacto"
+            description=" Nos puedes en contrar en nuestras redes sociales, correo electronico "
+            image={contactoImg}
             sx={{
               flexBasis: {
                 xs: '100%',
